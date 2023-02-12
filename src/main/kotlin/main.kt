d09cdd51da97d94d9fe202110a65f70ff367b456
Irina Derevyanko <wily@list.ru>
2023-02-12 18:40:28 +0300

    change post, new parameter

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,58 @@
+enum class ListPostType {
+    post, copy, reply, postpone, suggest
+}
+
+enum class TypeAttachment {
+    photo, video, file, link, audio
+}
+
+interface Attachment {
+    val type: TypeAttachment
+    val id: Int
+    val ownerId: Int
+    val title: String
+}
+
+class Photo(
+    override val type: TypeAttachment = TypeAttachment.photo,
+    override val id: Int,
+    override val ownerId: Int,
+    override val title: String,
+    val albumId: Int? = null
+) : Attachment {}
+
+class Video(
+    override val type: TypeAttachment,
+    override val id: Int,
+    override val ownerId: Int,
+    override val title: String,
+    val albumId: Int? = null
+) : Attachment {}
+
+class Audio(
+    override val type: TypeAttachment,
+    override val id: Int,
+    override val ownerId: Int,
+    override val title: String,
+    val albumId: Int? = null
+) : Attachment {}
+
+class File(
+    override val type: TypeAttachment,
+    override val id: Int,
+    override val ownerId: Int,
+    override val title: String,
+    val url: String
+) : Attachment {}
+
+class Link(
+    override val type: TypeAttachment,
+    override val id: Int,
+    override val ownerId: Int,
+    override val title: String,
+    val url: String
+) : Attachment {}
+
 data class CommentPost(
     val count: Long = 0,
     val canPost: Boolean = true, //информация о том, может ли текущий пользователь комментировать запись
@@ -17,18 +72,46 @@ data class Repost(
     val userRepost: Boolean = true //наличие репоста от текущего пользователя
 )
 
+data class Copyright(
+    val id: Int,
+    val link: String,
+    val name: String,
+    val type: String
+)
+
+data class Geo(
+    val type: String,
+    val coordinates: String,
+    val place: String
+)
+
 data class Post(
     val id: Int = 0,
     val ownerID: Int, //Идентификатор владельца стены, на которой размещена запись
     val fromID: Int, //Идентификатор автора записи
+    val date: String, // Время публикации записи в формате unixtime
     val friendsOnly: Boolean = false, //если запись была создана с опцией «Только для друзей
+    val replyOwnerId: Int? = null, //Идентификатор владельца записи, в ответ на которую была оставлена текущая.
+    val replyPostId: Int? = null, //Идентификатор записи, в ответ на которую была оставлена текущая
     val text: String,
-    val likes: Like = Like(0),
-    val comments: CommentPost = CommentPost(0),
-    val views: Int = 0,
-    val reposts: Repost = Repost(0),
+    val likes: Like? = null,
+    val comments: CommentPost? = null,
+    val views: Int? = null,
+    val reposts: Repost? = null,
     val canDelete: Boolean = true,
-    val canEdit: Boolean = true
+    val canEdit: Boolean = true,
+    val copyright: Copyright? = null, //Источник материала, объект с полями
+    val postType: ListPostType = ListPostType.post,
+    val geo: Geo? = null,
+    val attachment: Array<Video>? = null,
+    val signerI: Int? = null, //Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
+    val copyHistory: Array<Repost>? = null, //Массив, содержащий историю репостов для записи.
+    val canPin: Boolean = false, //Информация о том, может ли текущий пользователь закрепить запись
+    val isPinned: Boolean = false, //Информация о том, что запись закреплена.
+    val markedAsAds: Boolean = false, //Информация о том, содержит ли запись отметку «реклама
+    val postponedId: Int? = null, // Идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере.
+    val isFavorite: Boolean = false,
+    // Поля post_source, donut не добавляла
 
 )
 
@@ -79,8 +162,27 @@ object WallService {
 }
 
 fun main() {
-    WallService.addPost(Post(ownerID = 111, fromID = 222, text = "text1"))
-    WallService.addPost(Post(ownerID = 333, fromID = 444, text = "text2"))
-    WallService.updatePost(Post(id = 1, friendsOnly = true, text = "text3", ownerID = 0, fromID = 0))
+    WallService.addPost(Post(ownerID = 111, fromID = 222, date = "12.02.2020", replyOwnerId = 133, text = "text1"))
+    val video = Video(type = TypeAttachment.video, id = 1231, ownerId = 222, title = "audio")
+    WallService.addPost(
+        Post(
+            ownerID = 222,
+            fromID = 222,
+            date = "01.02.2023",
+            replyOwnerId = 13333,
+            text = "text1",
+            attachment = arrayOf(video)
+        )
+    )
+    WallService.updatePost(
+        Post(
+            id = 1,
+            ownerID = 0,
+            fromID = 0,
+            date = "14.02.2020",
+            friendsOnly = true,
+            text = "text3"
+        )
+    )
     print(WallService.returnListPost())
 }
--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -15,14 +15,23 @@ class WallServiceTest {
 
     @Test
     fun addPost() {
-        val post = WallService.addPost(Post(ownerID = 123, fromID = 222, text = "text1"))
-        val referenceRes = Post(id = 1, ownerID = 123, fromID = 222, text = "text1")
+        val post = WallService.addPost(Post(ownerID = 123, fromID = 222, text = "text1", date = "12.12.2022"))
+        val referenceRes = Post(id = 1, ownerID = 123, fromID = 222, text = "text1", date = "12.12.2022")
         assertEquals(referenceRes, post)
     }
 
     @Test
     fun updatePost() {
-        val result = WallService.updatePost(Post(id = 1, friendsOnly = true, text = "text3", ownerID = 0, fromID = 0))
+        val result = WallService.updatePost(
+            Post(
+                id = 1,
+                friendsOnly = true,
+                text = "text3",
+                ownerID = 0,
+                fromID = 0,
+                date = "12.12.2022"
+            )
+        )
         assertEquals(true, result)
     }
 }
